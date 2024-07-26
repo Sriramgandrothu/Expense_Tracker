b0086d2951cd8fd46429dd7f16ceb31291679cb7
sriramgandrothu <sriramgandorthu@gmail.com>
2024-07-26 22:32:05 +0530

    Test-EP

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import bg from './img/bg.png'
+import {MainLayout} from './Styles/Layouts'
+import Orb from './Components/Orb/Orb'
+import Navigation from './Components/Navigation/Navigation'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppStyled bg={bg} className="App">
+      <Orb />
+      <MainLayout>
+        <Navigation />
+      </MainLayout>
+
+      </AppStyled>
   );
 }
+
+const AppStyled = styled.div`
+  height: 100vh;
+   background-image: url(${props => props.bg});
+    position: relative;
+    main{
+      flex: 1;
+      background: rgba(252, 246, 249, 0.78);
+      border: 3px solid #FFFFFF;
+      backdrop-filter: blur(4.5px);
+      border-radius: 32px;
+      overflow-x: hidden;
+      &::-webkit-scrollbar{
+        width: 0;
+`;
 
 export default App;
